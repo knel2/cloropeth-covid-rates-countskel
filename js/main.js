8a7d0b3f486bbddc630c8b72577da9ba43bf5fa2
knel2 <knel2@uw.edu>
2026-01-27 23:08:25 -0800

    added functions to make work in map 1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia25lbDIiLCJhIjoiY21rdTlkYmx5MThyZjNmcHVrMDYzdXJ4dyJ9.HrXpvWOlXxo7fGCGnGQG6A';
+
+const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/light-v10', // style URL
+        zoom: 3, // starting zoom
+        center: [-100, 40] // starting center
+    }
+);
